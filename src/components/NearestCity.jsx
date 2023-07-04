f638f3161fc315ef00c72b1ba0701bfcd7050536
Momo-87 <momochristian87@gmail.com>
2023-07-04 10:27:09 +0100

    Refactore the code and delete SelectedCity.jsx component

--- src/components/NearestCity.jsx
+++ src/components/NearestCity.jsx
@@ -1,9 +1,15 @@
 import PropTypes from 'prop-types';
 import waterDrop from 'assets/icons/drop.png';
 
-const NearestCity = ({ city, isLoading, error }) => (
-  <div className="nearest-city-box">
-    {isLoading && (
+const NearestCity = ({
+  city, selectedCity, isLoading, error,
+}) => {
+  let currentCity = city;
+  if (selectedCity && Object.keys(selectedCity).length !== 0) currentCity = selectedCity;
+
+  return (
+    <div className="nearest-city-box">
+      {isLoading && (
       <div>
         is Loading
         <div className="lds-ripple">
@@ -11,57 +17,57 @@ const NearestCity = ({ city, isLoading, error }) => (
           <div>{}</div>
         </div>
       </div>
-    )}
-    {error && <p>{error}</p>}
-    {(!isLoading && !error && city && Object.keys(city).length !== 0)
+      )}
+      {error && <p>{error}</p>}
+      {(!isLoading && !error && currentCity && Object.keys(currentCity).length !== 0)
             && (
             <>
               <div className="weather-infos-box">
                 <div className="weather-icon-box">
-                  <img src={`https://download.spinetix.com/content/widgets/icons/weather/${city.weather.icon}.png`} alt="weather icon" />
+                  <img src={`https://download.spinetix.com/content/widgets/icons/weather/${currentCity.weather.icon}.png`} alt="weather icon" />
                 </div>
                 <div className="weather-data-box">
                   <div className="temp-box">
-                    {city.weather.temp}
+                    {currentCity.weather.temp}
                     °
                   </div>
                   <div className="hu-box">
                     <img src={waterDrop} alt="water drop" />
-                    {city.weather.hu}
+                    {currentCity.weather.hu}
                     %
                   </div>
                   <div className="ws-box">
                     ws:
                     {' '}
-                    {city.weather.ws}
+                    {currentCity.weather.ws}
                     m/s
                   </div>
                 </div>
               </div>
               <div className="air-data-box">
                 <h1 className="city">
-                  {city.city}
+                  {currentCity.city}
                   {' '}
                   Air Quality Index
                 </h1>
-                <p className={city.pollutionLevel}>
-                  {city.aqi}
+                <p className={currentCity.pollutionLevel}>
+                  {currentCity.aqi}
                 </p>
                 <p
-                  className={city.pollutionLevel}
+                  className={currentCity.pollutionLevel}
                 >
-                  {city.pollutionLevel}
+                  {currentCity.pollutionLevel}
                   <span className="pollutant">
                     PM2.5:
                     {' '}
-                    {city.pollution.pm2_5}
+                    {currentCity.pollution.pm2_5}
                     μg/m
                     <sup>3</sup>
                   </span>
                   <span className="pollutant">
                     PM10:
                     {' '}
-                    {city.pollution.pm10}
+                    {currentCity.pollution.pm10}
                     μg/m
                     <sup>3</sup>
                   </span>
@@ -69,13 +75,29 @@ const NearestCity = ({ city, isLoading, error }) => (
               </div>
             </>
             )}
-  </div>
-);
+    </div>
+  );
+};
 
 export default NearestCity;
 
 NearestCity.propTypes = {
   city: PropTypes.shape({
+    city: PropTypes.string,
+    pollutionLevel: PropTypes.string,
+    aqi: PropTypes.number,
+    weather: PropTypes.shape({
+      icon: PropTypes.string,
+      temp: PropTypes.number,
+      hu: PropTypes.number,
+      ws: PropTypes.number,
+    }),
+    pollution: PropTypes.shape({
+      pm2_5: PropTypes.number,
+      pm10: PropTypes.number,
+    }),
+  }).isRequired,
+  selectedCity: PropTypes.shape({
     city: PropTypes.string,
     pollutionLevel: PropTypes.string,
     aqi: PropTypes.number,
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,6 @@
 import NearestCity from 'components/NearestCity';
 import OtherCitiesList from 'components/OtherCitiesList';
-import SelectedCity from 'components/SelectedCity';
+// import SelectedCity from 'components/SelectedCity';
 import SearchBar from 'components/SearchBar';
 import Footer from 'components/Footer';
 import { getData } from 'redux/home/homeSlice';
@@ -13,8 +13,12 @@ const Home = () => {
   return (
     <div className="home">
       <SearchBar />
-      {selectedCity && Object.keys(selectedCity).length !== 0 ? <SelectedCity />
-        : <NearestCity city={city} isLoading={isLoading} error={error} /> }
+      <NearestCity
+        city={city}
+        selectedCity={selectedCity}
+        isLoading={isLoading}
+        error={error}
+      />
       <OtherCitiesList otherCities={otherCities} isLoading={isLoading} error={error} />
       <Footer />
     </div>
