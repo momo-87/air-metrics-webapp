130a2f0d16a242d089048599d7704aeed5c95e6d
Momo-87 <momochristian87@gmail.com>
2023-07-04 09:53:06 +0100

    Fix linter errors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => (
   <div className="footer">
     <p>Copyright© 2023</p>
-    <p>momochristian87@gmail.com</p>
+    <p className="email">momochristian87@gmail.com</p>
     <p>Yaounde, Cameroun</p>
     <p>Some rights reserved</p>
   </div>
