c04f894105d6b3c59182b295980d4d4b0032c3ec
Momo-87 <momochristian87@gmail.com>
2023-09-27 11:55:50 +0100

    preserve clickedCity data in the local storage

--- src/components/DetailsPage.jsx
+++ src/components/DetailsPage.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import { BsArrowLeftCircle } from 'react-icons/bs';
 import waterDrop from 'assets/icons/drop.png';
@@ -6,6 +7,14 @@ import { getData } from 'redux/home/homeSlice';
 
 const DetailsPage = () => {
   const { clickedCity } = useSelector(getData);
+  useEffect(() => {
+    if (Object.keys(clickedCity).length > 0) {
+      localStorage.setItem('clickedCity', JSON.stringify(clickedCity));
+    }
+  }, [clickedCity]);
+
+  const city = Object.keys(clickedCity).length > 0 ? clickedCity : JSON.parse(localStorage.getItem('clickedCity'));
+
   // Add pillution Level to clicked city
   const pollutionLevel = (aqi) => {
     let level = '';
@@ -26,15 +35,15 @@ const DetailsPage = () => {
         level = 'Very Poor';
         break;
       default:
-        clickedCity.pollutionLevel = '';
+        city.pollutionLevel = '';
     }
     return level;
   };
 
   return (
     <>
-      {clickedCity && Object.keys(clickedCity).length === 0 && <p className="no-data">Data Not Found</p>}
-      {clickedCity && Object.keys(clickedCity).length !== 0 && (
+      {city && Object.keys(city).length === 0 && <p className="no-data">Data Not Found</p>}
+      {city && Object.keys(city).length !== 0 && (
       <>
         <div className="details-section">
           <NavLink to="/" className="back-arrow-link">
@@ -42,50 +51,50 @@ const DetailsPage = () => {
           </NavLink>
           <div className="weather-infos-box">
             <div className="weather-icon-box">
-              <img src={`https://download.spinetix.com/content/widgets/icons/weather/${clickedCity.weather.icon}.png`} alt="weather icon" />
+              <img src={`https://download.spinetix.com/content/widgets/icons/weather/${city.weather.icon}.png`} alt="weather icon" />
             </div>
             <div className="weather-data-box">
               <div className="temp-box">
-                {clickedCity.weather.temp}
+                {city.weather.temp}
                 °
               </div>
               <div className="hu-box">
                 <img src={waterDrop} alt="water drop" />
-                {clickedCity.weather.hu}
+                {city.weather.hu}
                 %
               </div>
               <div className="ws-box">
                 ws:
                 {' '}
-                {clickedCity.weather.ws}
+                {city.weather.ws}
                 m/s
               </div>
             </div>
           </div>
           <div className="air-data-box">
             <h1 className="city">
-              {clickedCity.city}
+              {city.city}
               {' '}
               Air Quality Index
             </h1>
-            <p className={pollutionLevel(clickedCity.aqi)}>
-              {clickedCity.aqi}
+            <p className={pollutionLevel(city.aqi)}>
+              {city.aqi}
             </p>
             <p
-              className={pollutionLevel(clickedCity.aqi)}
+              className={pollutionLevel(city.aqi)}
             >
-              {pollutionLevel(clickedCity.aqi)}
+              {pollutionLevel(city.aqi)}
               <span className="pollutant">
                 PM2.5:
                 {' '}
-                {clickedCity.pollution.pm2_5}
+                {city.pollution.pm2_5}
                 μg/m
                 <sup>3</sup>
               </span>
               <span className="pollutant">
                 PM10:
                 {' '}
-                {clickedCity.pollution.pm10}
+                {city.pollution.pm10}
                 μg/m
                 <sup>3</sup>
               </span>
@@ -98,42 +107,42 @@ const DetailsPage = () => {
           <p className="pollutant">
             CO:
             {' '}
-            {clickedCity.pollution.co}
+            {city.pollution.co}
             μg/m
             <sup>3</sup>
           </p>
           <p className="pollutant">
             NO:
             {' '}
-            {clickedCity.pollution.no}
+            {city.pollution.no}
             μg/m
             <sup>3</sup>
           </p>
           <p className="pollutant">
             NO2:
             {' '}
-            {clickedCity.pollution.no2}
+            {city.pollution.no2}
             μg/m
             <sup>3</sup>
           </p>
           <p className="pollutant">
             O3:
             {' '}
-            {clickedCity.pollution.o3}
+            {city.pollution.o3}
             μg/m
             <sup>3</sup>
           </p>
           <p className="pollutant">
             SO2:
             {' '}
-            {clickedCity.pollution.so2}
+            {city.pollution.so2}
             μg/m
             <sup>3</sup>
           </p>
           <p className="pollutant">
             NH3:
             {' '}
-            {clickedCity.pollution.nh3}
+            {city.pollution.nh3}
             μg/m
             <sup>3</sup>
           </p>
